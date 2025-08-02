1b768236fbad296eec5a763bb840286a5168bd7c
rafagarrido <garrido.ochoa@gmail.com>
2025-08-02 18:15:49 +0200

    Update DisplayForm.Colorize.js

--- SP.WebScripts/CSR/DisplayForm.Colorize.js
+++ SP.WebScripts/CSR/DisplayForm.Colorize.js
@@ -1,1 +1,29 @@
+SP.SOD.executeFunc('clienttemplates.js', 'SPClientTemplates', function () {
+    (function () {
+        var overrideCtx = {};
+        overrideCtx.Templates = {};
+
+        overrideCtx.Templates.Fields = {
+            'Color': {
+                'View': colorFieldRenderer
+            }
+        };
+
+        SPClientTemplates.TemplateManager.RegisterTemplateOverrides(overrideCtx);
+
+        function colorFieldRenderer(ctx) {
+            var color = ctx.CurrentItem[ctx.CurrentFieldSchema.Name];
+
+            var colorMap = {
+                'Rojo': '#ffcccc',
+                'Verde': '#ccffcc',
+                'Azul': '#cce5ff'
+            };
+
+            var bgColor = colorMap[color] || '#f0f0f0';
+
+            return "<div style='padding:5px; font-weight:bold; background-color:" + bgColor + ";'>" + color + "</div>";
+        }
+    })();
+});
 
