badfcfc65f5de34b7948cf4e2d77d06ae88ba91b
rafagarrido <garrido.ochoa@gmail.com>
2025-08-11 08:17:15 +0200

    Update optimizarfechas.js

--- SP.WebScripts/CSR/optimizarfechas.js
+++ SP.WebScripts/CSR/optimizarfechas.js
@@ -1,10 +1,3 @@
-
-// Asegúrate de cargar moment.js antes de usarlo
-// Puedes incluirlo así en tu CSR o página:
-// <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.4/moment.min.js"></script>
-// <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.4/locale/es.min.js"></script>
-
-// Convierte fecha en formato español de SharePoint a ISO
 function normalizarFechaSharePoint(fechaSP) {
     if (!fechaSP) return null;
 
